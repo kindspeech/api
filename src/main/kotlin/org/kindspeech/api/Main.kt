9aa2129a52a264f5c85172ab0589e317c99f9d8d
Nathanael Silverman <nathanael.silverman@gmail.com>
2020-07-20 16:42:39 -0700

    Create /messages endpoint

--- src/main/kotlin/org/kindspeech/api/Main.kt
+++ src/main/kotlin/org/kindspeech/api/Main.kt
@@ -7,14 +7,22 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.json.JSONArray
 
 fun main() {
-    val target = System.getenv("TARGET") ?: "World"
     val port = System.getenv("PORT") ?: "8080"
     embeddedServer(Netty, port.toInt()) {
         routing {
-            get("/") {
-                call.respondText("Hello $target", ContentType.Text.Html)
+            get("/messages") {
+                val limit = call.request.queryParameters["limit"]?.toInt() ?: 1
+                val response = JSONArray().apply {
+                    (0 until limit).forEach {
+                        put("You are wonderful.")
+                    }
+                }
+                call.respondText(ContentType.Application.Json) {
+                    response.toString()
+                }
             }
         }
     }.start(wait = true)
